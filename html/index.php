c3a4d5affc68d27c418cadb3b1cc5154ec970f11
Nate Jones <nate@endot.org>
2014-06-20 20:49:17 -0700

    testing bootstrap downloading working

--- html/index.php
+++ html/index.php
@@ -1,3 +1,11 @@
 <?
-header("Location: https://github.com/justone/pmb");
+
+$path = $_SERVER['REQUEST_URI'];
+
+if ($path == '/testing/bootstrap') {
+    header("Location: /2014-06-19-0630-d23dd81/bootstrap");
+} else {
+    # TODO change to version listing
+    header("Location: https://github.com/justone/pmb");
+}
 ?>
