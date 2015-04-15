86a8a43c86cda6f61330331bcb9cb6b49afb4ca1
Nate Jones <nate@endot.org>
2015-04-14 20:07:10 -0700

    get to / pulls latest bootstrap

--- html/index.php
+++ html/index.php
@@ -2,8 +2,8 @@
 
 $path = $_SERVER['REQUEST_URI'];
 
-if ($path == '/testing/bootstrap') {
-    header("Location: /2014-06-19-0630-d23dd81/bootstrap");
+if ($path == '/') {
+    header("Location: /latest/bootstrap");
 } else {
     # TODO change to version listing
     header("Location: https://github.com/justone/pmb");
