e9e8f0b6145c06086ef3a520ba63452fca55b729
Nate Jones <nate@endot.org>
2015-04-14 20:28:54 -0700

    MVP download listing and instructions

--- html/index.php
+++ html/index.php
@@ -2,10 +2,29 @@
 
 $path = $_SERVER['REQUEST_URI'];
 
-if ($path == '/') {
+if ($path == '/latest') {
     header("Location: /latest/bootstrap");
 } else {
-    # TODO change to version listing
-    header("Location: https://github.com/justone/pmb");
+    $prior_versions = glob("201*");
+    rsort($prior_versions);
+    $latest = array_shift($prior_versions);
+?>
+<p>To get latest bootstrap:</p>
+<code>curl -L <?= $_SERVER['HTTP_HOST'] ?>/latest</code>
+<p> or </p>
+<code>curl <?= $_SERVER['HTTP_HOST'] ?>/<?= $latest ?>/bootstrap</code>
+<p>Prior versions:</p>
+<?
+?>
+    <ul>
+<?
+    foreach ($prior_versions as $version) {
+?>
+        <li><a href="<?= $version ?>"><?= $version ?></a></li>
+<?
+    }
+?>
+    </ul>
+<?
 }
 ?>
